c8171fe2164a23668e09916ea8da1f4713b8dac2
MidoriYano <75301664+MidoriYano@users.noreply.github.com>
2026-05-21 17:59:38 +0900

    Update streamlit_app.py to input user information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("座標の読み取りに関する評価実験")
+st.write("Input your informaton!")
+
+student_id = st.text_input("学生番号を入力してください")
+
+field = st.radio(
+    "あなたの所属を選んでください",
+    ["文系", "理系"]
 )
+
+
+if st.button("次へ"):
+    if student_id == "":
+        st.warning("学生番号を入力してください。")
+    else:
+        st.success("入力が完了しました。")
+        st.write("番号：", student_id)
+        st.write("所属：", field)
+        st.write("次の画面からは座標を表示していきます。")
