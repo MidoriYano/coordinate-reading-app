0c6f708adebbf589c4e4333fdc16440e6b25ca87
MidoriYano <75301664+MidoriYano@users.noreply.github.com>
2026-05-21 18:28:08 +0900

    Update streamlit_app.py to show an image

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 st.title("座標の読み取りに関する評価実験")
-st.write("Input your informaton!")
+
+if "page" not in st.session_state:
+    st.session_state.page = "start"
+
+if st.session_state.page == "start":
+    st.write("Input your informaton!")
 
 student_id = st.text_input("学生番号を入力してください")
 
@@ -15,7 +20,24 @@ if st.button("次へ"):
     if student_id == "":
         st.warning("学生番号を入力してください。")
     else:
-        st.success("入力が完了しました。")
-        st.write("学生番号：", student_id)
-        st.write("専攻：", field)
-        st.write("次の画面からは座標を表示していきます。")
+        # st.success("入力が完了しました。")
+        # st.write("学生番号：", student_id)
+        # st.write("専攻：", field)
+        # st.write("次の画面からは座標を表示していきます。")
+         st.session_state.student_id = student_id
+            st.session_state.field = field
+            st.session_state.page = "image"
+            st.rerun()
+
+elif st.session_state.page == "image":
+    st.write("画像を見て座標に回答してください。")
+
+    st.image("https://placehold.co/600x400?text=Sample+Image")
+
+    answer = st.text_area("座標値この画像について気づいたことを書いてください")
+
+    if st.button("次の画像へ"):
+        st.success("次の画像に進む予定です。")
+        st.write("番号：", st.session_state.student_id)
+        st.write("所属：", st.session_state.field)
+        st.write("回答：", answer)
